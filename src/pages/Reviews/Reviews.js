523b477570cd858e37aa2445da549eb7c59309d6
itarek99 <tarekulislam7060@gmail.com>
2022-11-09 21:37:56 +0600

    review delete button added

--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -12,12 +12,37 @@ const Reviews = () => {
       .then((data) => setReviews(data));
   }, [user.email]);
 
+  const handleDelete = (id) => {
+    const confirmDelete = window.confirm('Are you sure?');
+    if (!confirmDelete) return;
+
+    fetch(`http://localhost:5000/orders/${id}`, { method: 'DELETE' })
+      .then((res) => res.json())
+      .then((data) => {
+        const reviewsAfterDelete = reviews.filter((reviews) => reviews._id !== id);
+        setReviews(reviewsAfterDelete);
+
+        if (data.deletedCount > 0) {
+          alert('Review delete successfully!');
+        }
+      });
+  };
+
   return (
-    <div className='container mx-auto px-2 my-12'>
+    <div className='container mx-auto px-2 my-8 min-h-[70vh]'>
       <div className='max-w-4xl mx-auto'>
-        {reviews.map((review) => (
-          <Review key={review._id} review={review} />
-        ))}
+        {reviews.length === 0 ? (
+          <div className='h-[70vh] flex items-center justify-center'>
+            <p className='text-3xl uppercase font-bold'>No reviews were added</p>
+          </div>
+        ) : (
+          <div>
+            <h2 className='font-bold text-3xl mb-8'>My Reviews</h2>
+            {reviews.map((review) => (
+              <Review key={review._id} review={review} handleDelete={handleDelete} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/Reviews/components/Review.js
+++ src/pages/Reviews/components/Review.js
@@ -1,11 +1,18 @@
-import { FaExternalLinkSquareAlt } from 'react-icons/fa';
+import { FaEdit, FaExternalLinkSquareAlt, FaTrash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const Review = ({ review }) => {
-  const { review_for, review_text, service_id } = review;
+const Review = ({ review, handleDelete }) => {
+  const { _id, review_for, review_text, service_id } = review;
 
   return (
-    <div className='bg-neutral mb-6 p-8 rounded-2xl'>
+    <div className='relative bg-neutral mb-6 p-8 rounded-2xl'>
+      <div className='absolute top-6 right-6 flex items-center gap-4'>
+        <FaEdit />
+        <button onClick={() => handleDelete(_id)} className='text-error'>
+          <FaTrash />
+        </button>
+      </div>
+
       <h3 className='text-xl font-bold flex items-center mb-2'>
         <span className='mr-2'>{review_for}</span>
         <Link className='text-base' to={`/services/${service_id}`}>
