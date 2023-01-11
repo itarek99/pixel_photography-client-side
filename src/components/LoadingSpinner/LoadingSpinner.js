6c6a15e8bb52858304a6e625b1c4f4c034cd9067
itarek99 <tarekulislam7060@gmail.com>
2023-01-11 18:29:01 +0600

    loading spinner updated

--- src/components/LoadingSpinner/LoadingSpinner.js
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,17 +1,13 @@
 const LoadingSpinner = () => {
   return (
-    <div className='flex items-center justify-center w-full h-screen fixed inset-0 bg-black/25'>
-      <div aria-label='Loading...' role='status'>
-        <svg className='h-16 w-16 animate-spin' viewBox='3 3 18 18'>
-          <path
-            className='fill-secondary'
-            d='M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z'
-          ></path>
-          <path
-            className='fill-primary'
-            d='M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z'
-          ></path>
-        </svg>
+    <div className='loader'>
+      <div className='loadingio-spinner-dual-ring-11p0rfzzq2m'>
+        <div className='ldio-sv1ci8a4l9p'>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+        </div>
       </div>
     </div>
   );
